b78d9f7e9220b35c5fdae723fda2cc77a2bce7e8
GustavJaner <gustav.janer@gmail.com>
2019-11-19 14:16:00 +0100

    Setup

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const mongoose = require("mongoose"); //using mongoose to connect with db
+
+const schema = require("./schema/schema");
+const db = require('./config').mongoURI
+
+
+const app = express();
+
+mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser:true })
+.then(() => console.log("DB connected"))
+.catch(err => console.log(err))
+
+/*
+We can use graphql on express server with middlewares, so that whenever
+we need graphql query from frontend, our express server can handle it
+smoothly.
+*/
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true
+  })
+);
+
+app.listen(4000, () => {
+ console.log("Server is listening on port 4000");
+});
---
+++ server/config/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://melker:melker@cluster0-shard-00-00-b53ef.mongodb.net:27017,cluster0-shard-00-01-b53ef.mongodb.net:27017,cluster0-shard-00-02-b53ef.mongodb.net:27017/cluster0?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+}
